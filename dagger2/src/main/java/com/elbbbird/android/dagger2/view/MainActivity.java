fffbe7c2e9fdade5499733ff8c2d8925716143d7
hailong0707 <hailong0707@gmail.com>
2016-03-28 23:59:11 +0800

    integrate dagger2, a fast dependency injector for Android and Java.

--- dagger2/src/main/java/com/elbbbird/android/dagger2/view/MainActivity.java
+++ dagger2/src/main/java/com/elbbbird/android/dagger2/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.elbbbird.android.dagger2;
+package com.elbbbird.android.dagger2.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.elbbbird.android.dagger2.R;
 
 public class MainActivity extends AppCompatActivity {
 
